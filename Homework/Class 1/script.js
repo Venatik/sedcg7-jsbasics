7076e558f53183efdaaa609f14ce5a104ab4d0b0
Venatik <stefan.trajkovski008@gmail.com>
2023-12-05 11:58:15 +0100

    modified:   Homework/Class 1/script.js

--- Homework/Class 1/script.js
+++ Homework/Class 1/script.js
@@ -1,9 +1,11 @@
 var phones = 30;
 var price = 119.95;
-var tax = 5 / 100 * 119.5
-console.log(tax)
+//Calculating tax per item
+var tax = 5 / 100 * 119.5;
+console.log(tax);
+//Calculating price per phone
 var pricePerPhone = (price + tax);
-console.log(pricePerPhone)
-
-var result = pricePerPhone * phones
+console.log(pricePerPhone);
+//Calculating price per # of phones
+var result = pricePerPhone * phones;
 console.log(`The price for 30 phones is: $${result}.`);

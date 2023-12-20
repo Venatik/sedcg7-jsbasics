4427308016771094bfe5029e52afe92e22a8b918
Venatik <stefan.trajkovski008@gmail.com>
2023-12-20 19:46:38 +0100

    modified:   Class 7/Student Registry Form/script.js

--- Class 7/Student Registry Form/script.js
+++ Class 7/Student Registry Form/script.js
@@ -1,66 +1,68 @@
-// FIX THIS CODE
+let students = [];
 
-// let students = [];
+function Student(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+}
 
-// function Student(firstName, lastName, age) {
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-//     this.age = age;
-// }
+let button = document.getElementById("addButton");
+let list = document.getElementById("list");
 
-// function showStudents(studentsArr, element) {
-//     element.innerHTML = "";
-//     for(let student of studentsArr) {
-//         element.innerHTML += `<li><b>Student:</b<${student.firstName} ${student.lastName} - ${student.age} years old</li>`;
-        
-//     }
+function showStudents(studentsArr, element) {
+    element.innerHTML = "";
+    for(let student of studentsArr) {
+        element.innerHTML += `<li><b>Student: </b> ${student.firstName} ${student.lastName}. <b>Age: </b>${student.age}</li>`
+    }
+}
 
-// let button = document.getElementById("addButton");
-// let studentList = document.getElementById("studentList");
 
-// button.addEventListener("click", function() {
-//     event.preventDefault();
-//     let firstName = document.getElementById("firstName").value;
-//     let lastName = document.getElementById("lastName").value;
-//     let age = document.getElementById("age").value;
-//     let newStudent = new Student(firstName.value, lastName.value, age).value;
-//     students.push(newStudent);
-//     // console.log(students);
-//     showStudents(students, studentList);
+button.addEventListener("click", function(event) {
+    event.preventDefault();
 
-//     firstName.value = "";
-//     lastName.value = "";
-//     age.value = "";
-// })};
+    let name = document.getElementById("firstName");
+    let lastName = document.getElementById("lastName");
+    let age = document.getElementById("age");
+
+    let newStudent = new Student(name.value, lastName.value, age.value);
+
+    students.push(newStudent);
+    console.log(students);
+    showStudents(students, list);
+
+    name.value = "";
+    lastName.value = "";
+    age.value = ""
+})
 
 
 // Alternative solution:
 
-let students = [];
-function saveStudent() {
-    let firstName = document.getElementById("firstName").value;
-    let lastName = document.getElementById("lastName").value;
-    let age = document.getElementById("age").value;
+// let students = [];
+// function saveStudent() {
+//     let firstName = document.getElementById("firstName").value;
+//     let lastName = document.getElementById("lastName").value;
+//     let age = document.getElementById("age").value;
 
-    let student = {
-        firstName,
-        lastName,
-        age
-    };
+//     let student = {
+//         firstName,
+//         lastName,
+//         age
+//     };
 
-    students.push(student);
-    console.log(students);
+//     students.push(student);
+//     console.log(students);
 
-    let studentList = document.getElementById("studentList");
-    let listItem = document.createElement("li");
-    listItem.innerHTML = `<b>Student: </b>${firstName} ${lastName} - ${age} years old`;
-    studentList.appendChild(listItem);
-    console.log(listItem);
+//     let studentList = document.getElementById("studentList");
+//     let listItem = document.createElement("li");
+//     listItem.innerHTML = `<b>Student: </b>${firstName} ${lastName} - ${age} years old`;
+//     studentList.appendChild(listItem);
+//     console.log(listItem);
 
-    document.getElementById("firstName").value = "";
-    document.getElementById("lastName").value = "";
-    document.getElementById("age").value = "";
-}
+//     document.getElementById("firstName").value = "";
+//     document.getElementById("lastName").value = "";
+//     document.getElementById("age").value = "";
+// }
 
-let addButton = document.getElementById("addButton");
-addButton.addEventListener("click", saveStudent);
+// let addButton = document.getElementById("addButton");
+// addButton.addEventListener("click", saveStudent);
